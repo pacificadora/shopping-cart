854cd48fd00de260025b4d86d9fb69b89572aabd
Aman Singhal <singhalaman198@gmail.com>
2024-10-18 09:02:29 +0530

    added imports and created total amount calculation

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,20 +1,36 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CartItem from "../components/CartItem";
+import { useEffect, useState } from "react";
 
 const Cart = () => {
   const { cart } = useSelector((state) => { return state });
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0))
+  }, [cart])
   return (
     <div>
       {
         cart.length > 0 ?
           (
             <div>
-              {
-                cart.map((item, index) => {
-                  return <CartItem key={item.id} item={item} itemIndex={item}></CartItem>
-                })
-              }
+              <div>
+                {
+                  cart.map((item, index) => {
+                    return <CartItem key={item.id} item={item} itemIndex={item}></CartItem>
+                  })
+                }
+              </div>
+              <div>
+                <div>Your Cart</div>
+                <div>Summary</div>
+                <p><span>Total Items: {cart.length}</span></p>
+              </div>
+              <div>
+                <p>Total Amount: {totalAmount}</p>
+              </div>
             </div>
           ) :
           (
