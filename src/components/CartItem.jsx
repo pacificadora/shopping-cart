59da61354f6cd34939df410825b0a697e3afed78
Aman Singhal <singhalaman198@gmail.com>
2024-10-18 07:41:24 +0530

    creating cart item page

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { FcDeleteDatabase } from 'react-icons/fc'
 
-const CartItem = () => {
+const CartItem = ({ item, index }) => {
   return (
-    <div>CartItem</div>
+    <div>
+      <div>
+        <div>
+          <img src={item.image} />
+        </div>
+        <div>
+          <h1>{item.tile}</h1>
+          <h1>{item.description}</h1>
+          <div>
+            <p>{item.price}</p>
+            <div>
+              <FcDeleteDatabase />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
